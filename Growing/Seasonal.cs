30616ed640236a9e00d9c4ebf068b1bd004b3fca
DarkEyeDragon <mccrafter4liver@gmail.com>
2023-05-08 21:53:03 +0200

    Fix hibernate state

--- Growing/Seasonal.cs
+++ Growing/Seasonal.cs
@@ -2,7 +2,9 @@
 using Timberborn.EntitySystem;
 using Timberborn.Growing;
 using Timberborn.NaturalResourcesModelSystem;
+using Timberborn.NaturalResourcesMoisture;
 using Timberborn.Persistence;
+using Timberborn.PrefabSystem;
 using Timberborn.SoilMoistureSystem;
 using UnityEngine;
 
@@ -15,15 +17,23 @@ public class Seasonal : MonoBehaviour
         var hibernateObject = GetComponent<HibernateObject>();
         var growable = GetComponent<Growable>();
         var dryObject = GetComponent<DryObject>();
+        var prefab = GetComponent<Prefab>();
+        var naturalResourceLifecycleModel = GetComponent<NaturalResourceModel>();
+        //Pine trees dont hibernate. So we dont need to listen to the event.
+        if(prefab.PrefabName.Equals("Pine", StringComparison.CurrentCultureIgnoreCase))
+            return;
         hibernateObject.EnteredHibernateState += (_, _) =>
         {
-            growable.PauseGrowing();
+            //growable.PauseGrowing();
             dryObject.EnterDryState();
+            naturalResourceLifecycleModel.ShowCurrentModel();
+            
         };
         hibernateObject.ExitedHibernateState += (_, _) =>
         {
-            growable.ResumeGrowing();
-            dryObject.EnterDryState();
+            //growable.ResumeGrowing();
+            dryObject.ExitDryState();
+            naturalResourceLifecycleModel.ShowCurrentModel();
         };
     }
 }
